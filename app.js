0236b891fbc1c235a093c9049eeb9e141d09479b
Bakhtimes <ailpokabdibek@gmail.com>
2024-10-02 12:21:26 +0500

    asd

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const {Web3} = require('web3');
 const web3 = new Web3('https://mainnet.infura.io/v3/c4911b8f65944a988495761c883aaea3');
+a = 3;
 
 web3.eth.getBalance("0x407d73d8a49eeb85d32cf465507dd71d507100c1").then(console.log);
